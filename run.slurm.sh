06502b710a8ce01a417734f0bd88434e07e8a52d
Howard Lau <howardlau1999@hotmail.com>
2021-01-30 17:41:40 +0800

    Add slurm scripts

---
+++ run.slurm.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+python -m torch.distributed.launch \
+  --use_env \
+  --nproc_per_node=4 \
+  --nnodes=${SLURM_JOB_NUM_NODES} \
+  --node_rank=${SLURM_NODEID} \
+  --master_addr=${MASTER_ADDR} \
+  --master_port=${MASTER_PORT} \
+  ddp.py
